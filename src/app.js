def7244e4c2d77b0ce998e160ca152569ef162b6
Pamela Lozano <pamelalozano16@gmail.com>
2022-09-19 19:01:47 -0500

    Csv to json library test

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const path = require("path");
 const hbs = require("hbs");
+const csv = require("csvtojson");
+const { json } = require("express/lib/response");
 const port = 3000;
 const publicPath = path.join(__dirname, "../public");
+const csvPath = path.join(__dirname, "../public/directory.csv");
 const templatePath = path.join(__dirname, "../templates");
 const partialsPath = path.join(__dirname, "../templates/partials");
 
@@ -14,8 +17,20 @@ app.set("views", templatePath);
 app.use(express.static(publicPath));
 hbs.registerPartials(partialsPath);
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  let jsonArray = [];
+  try {
+    jsonArray = await csv().fromFile(csvPath);
+    jsonArray.map((res) => {
+      //Esto es para las respuestas de checklist
+      res.Checklist = res.Checklist.split(";");
+    });
+    console.log(jsonArray);
+  } catch (error) {
+    console.log(error);
+  }
+
+  res.render("index", { jsonArray });
 });
 
 app.listen(port, () => {
